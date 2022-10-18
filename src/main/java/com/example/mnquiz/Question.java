8c92c5c0f245a519f4d059f2473b60d40f41e71a
crossfade69 <73494330+crossfade69@users.noreply.github.com>
2022-10-18 16:52:30 +0200

    Add files via upload

---
+++ src/main/java/com/example/mnquiz/Question.java
@@ -0,0 +1,18 @@
+package com.example.mnquiz;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId,boolean trueAnswer){
+        this.questionId=questionId;
+        this.trueAnswer=trueAnswer;
+    }
+    public boolean isTrueAnswer(){
+        return this.trueAnswer;
+    }
+
+    public int getQuestionId() {
+        return this.questionId;
+    }
+}
