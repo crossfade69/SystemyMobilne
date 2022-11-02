616120d0893728f265ef4a1bfc9fd3ecd269f6db
crossfade69 <73494330+crossfade69@users.noreply.github.com>
2022-11-02 21:11:40 +0100

    Add files via upload

---
+++ src/main/java/com/example/smzad3/TaskListActivity.java
@@ -0,0 +1,13 @@
+package com.example.smzad3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import android.os.Bundle;
+
+public class TaskListActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
---
+++ src/main/java/com/example/smzad3/TaskStorage.java
@@ -0,0 +1,35 @@
+package com.example.smzad3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TaskStorage {
+    private static final TaskStorage taskStorage = new TaskStorage();
+
+    private final List<Task> tasks;
+
+    public static  TaskStorage getInstance() { return taskStorage; }
+
+    private TaskStorage() {
+        tasks = new ArrayList<>();
+        for(int i = 1; i <= 150; i++) {
+            Task task = new Task();
+            task.setName("Zadanie nr: " + i);
+            task.setDone(i % 3 == 0);
+            tasks.add(task);
+        }
+    }
+    public Task getTask(UUID taskID) {
+        Task taskFound = new Task();
+        for(Task task : tasks){
+            if(task.getID().equals(taskID)) taskFound = task;
+        }
+        System.out.println("Found task: " + taskFound.getName());
+        return taskFound;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+}
