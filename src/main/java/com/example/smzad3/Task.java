6afdbb7bf0304da0290d3d1c40fa61c5adb8682a
crossfade69 <73494330+crossfade69@users.noreply.github.com>
2022-11-02 17:40:15 +0100

    Add files via upload

---
+++ src/main/java/com/example/smzad3/Task.java
@@ -0,0 +1,40 @@
+package com.example.smzad3;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Task {
+    private UUID id;
+    private String name;
+    private Date date;
+    private Boolean done;
+
+    public Task() {
+        this.id = UUID.randomUUID();
+        this.date = new Date();
+    }
+
+    public UUID getID() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public Boolean isDone() {
+        return this.done;
+    }
+
+    public void setDone(Boolean checked) {
+        this.done = checked;
+    }
+}
